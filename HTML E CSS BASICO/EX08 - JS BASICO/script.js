a4d9667e1aa598b945df12025ef2e48977df34c3
TabGuima <tab.guimaraes@gmail.com>
2023-02-07 07:58:02 -0300

    exercicios queryselector

---
+++ HTML E CSS BASICO/EX08 - JS BASICO/script.js
@@ -0,0 +1,33 @@
+// FUNÇÕES
+
+// function dadosLivro(nome, ano, autor) {
+//   return {
+//     nome: nome,
+//     totalAnos: 2050 - ano,
+//     autor: autor,
+//     frase: nome.toUpperCase() + " por " + autor.toUpperCase(),
+//   };
+// }
+// const retornoLivro = dadosLivro("o senhor dos anéis", 1954, "j. r. r. tolkien");
+// console.log(retornoLivro);
+// console.log(retornoLivro.frase);
+//
+
+// const curso = {
+//   nome: "Javascript",
+//   completar() {
+//     console.log("Curso completo de " + curso.nome);
+//   },
+// };
+
+// curso.completar()
+
+const ativar = document.querySelector(".modo");
+console.log(ativar);
+console.log("Teste");
+
+// function ativar() {
+//   document.body.style.color = "black";
+// }
+
+// modoNoturno.addEventListener("click", ativar);
