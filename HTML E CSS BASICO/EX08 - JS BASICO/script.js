d2532e45ecf8984fd45fb6e44e5e86ecfec84a9d
TabGuima <tab.guimaraes@gmail.com>
2023-02-08 12:33:17 -0300

    testes ex08

--- HTML E CSS BASICO/EX08 - JS BASICO/script.js
+++ HTML E CSS BASICO/EX08 - JS BASICO/script.js
@@ -22,14 +22,26 @@
 
 // curso.completar()
 
-const coordenadas = window.innerWidth;
+// const coordenadas = window.innerWidth;
 
-window.addEventListener("mousemove", posicao);
+// window.addEventListener("mousemove", posicao);
 
-function posicao(event) {
-  const pos = {
-    x: event.x,
-    y: event.y,
-  };
-  console.log(pos);
+// function posicao(event) {
+//   const pos = {
+//     x: event.x,
+//     y: event.y,
+//   };
+//   console.log(pos);
+// }
+
+function posicao() {
+  window.addEventListener("mousemove", seguirMouse);
+  const circulo = document.querySelector(".circulo");
+
+  function seguirMouse(event) {
+    circulo.style.top = event.y + "px";
+    circulo.style.left = event.x + "px";
+    // console.log(event.x);
+  }
 }
+posicao();
