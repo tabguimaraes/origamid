ce155a74364970c2615f5e2e452a24d3d8635b7f
TabGuima <tab.guimaraes@gmail.com>
2023-02-10 15:25:06 -0300

    teste de resolução do exercicio

--- HTML E CSS BASICO/EX08 - JS BASICO/script.js
+++ HTML E CSS BASICO/EX08 - JS BASICO/script.js
@@ -34,14 +34,31 @@
 //   console.log(pos);
 // }
 
-function posicao() {
-  window.addEventListener("mousemove", seguirMouse);
-  const circulo = document.querySelector(".circulo");
+// function posicao() {
+//   window.addEventListener("mousemove", seguirMouse);
+//   const circulo = document.querySelector(".circulo");
 
-  function seguirMouse(event) {
-    circulo.style.top = event.y + "px";
-    circulo.style.left = event.x + "px";
-    // console.log(event.x);
-  }
-}
-posicao();
+//   function seguirMouse(event) {
+//     circulo.style.top = event.y + "px";
+//     circulo.style.left = event.x + "px";
+//     // console.log(event.x);
+//   }
+// }
+// posicao();
+
+// const elemento = document.querySelector("body");
+// const elementoNaoExiste = document.querySelector("h1");
+
+// console.log("Elemento existe?: " + Boolean(elemento));
+// console.log("Elemento existe?: " + Boolean(elementoNaoExiste));
+
+const resultado = document.querySelector(".resultado");
+const botao = document.querySelector("button");
+
+console.log(resultado);
+
+// if (resultado) {
+//   console.log("teste ok");
+// } else {
+//   console.log("não ok");
+// }
