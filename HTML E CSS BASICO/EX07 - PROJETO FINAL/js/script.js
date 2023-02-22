0193688b446b8872a22b28bd0356911a4062edbd
TabGuima <tab.guimaraes@gmail.com>
2023-02-22 08:00:38 -0300

    add function ativarlink

--- HTML E CSS BASICO/EX07 - PROJETO FINAL/js/script.js
+++ HTML E CSS BASICO/EX07 - PROJETO FINAL/js/script.js
@@ -1,1 +1,13 @@
 console.log("OK");
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(linkAtivo) {
+  const url = location.href;
+  const href = linkAtivo.href;
+  //   console.log(href);
+  //   console.log("");
+  console.log(url);
+}
+
+links.forEach(ativarLink);
