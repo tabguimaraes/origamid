88aa0b6627644acd87c52257a45de0ca040bb991
TabGuima <tab.guimaraes@gmail.com>
2023-04-03 19:13:57 -0300

    mudanca de diretorios de pastas

--- HTML E CSS BASICO/JAVASCRIPT/PROJETO 1/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-console.log("OK");
-
-const pagina = window;
-const h1 = document.querySelector(".titulo");
-console.log(h1);
-
-console.log(window);
---
+++ JAVASCRIPT/PROJETO 1/assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("Conexão ok");
+
+const animais = document.getElementById("animais");
+
+// if (animais) {
+//   console.log("Ok");
+// }
+const gridSection = document.getElementsByClassName("grid-section");
+
