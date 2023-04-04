d691ca24f719bdd570cdd1e774f18023c46dedc4
TabGuima <tab.guimaraes@gmail.com>
2023-04-04 08:06:49 -0300

    aula foreach

--- JAVASCRIPT/PROJETO 1/assets/js/script.js
+++ JAVASCRIPT/PROJETO 1/assets/js/script.js
@@ -15,19 +15,31 @@ const html = document.querySelector("html");
 // EXERCICIOS
 // Retorne no console todas as imagens do site
 
-console.log(html.querySelectorAll("img"));
-console.log("");
-console.log(html.getElementsByTagName("img"));
+// console.log(html.querySelectorAll("img"));
+// console.log("");
+// console.log(html.getElementsByTagName("img"));
 
-// Retorne no console apenas as imagens que começaram com a palavra imagem
+// // Retorne no console apenas as imagens que começaram com a palavra imagem
 
-console.log(html.querySelectorAll("[src*='imagem']"));
+// console.log(html.querySelectorAll("[src*='imagem']"));
 
-// Selecione todos os links internos (onde o href começa com #)
-console.log(html.querySelectorAll("[href*='#']"));
+// // Selecione todos os links internos (onde o href começa com #)
+// console.log(html.querySelectorAll("[href*='#']"));
 
-// Selecione o primeiro h2 dentro de .animais-descricao
-console.log(html.querySelector(".animais-descricao h2"));
+// // Selecione o primeiro h2 dentro de .animais-descricao
+// console.log(html.querySelector(".animais-descricao h2"));
 
-// Selecione o último p do site
-console.log(html.querySelector(".copy p"));
+// // Selecione o último p do site
+// console.log(html.querySelectorAll(".copy p"));
+
+// const footer = html.querySelectorAll(".copy p");
+// // console.log(footer);
+
+// footer.forEach((item) => {
+//   //   item.classList.add("ativado");
+//   //   console.log(item);
+//   if (item.classList.toggle("ativado")) {
+//     item.innerHTML = "Novo paragrafo";
+//   }
+//   console.log(item);
+// });
