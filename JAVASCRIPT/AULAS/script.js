7ceff7d9ac918d0c2741922dcb3e4e2214be7dba
TabGuima <tab.guimaraes@gmail.com>
2023-06-25 10:19:26 -0300

    revisao loop for e foreach

--- JAVASCRIPT/AULAS/script.js
+++ JAVASCRIPT/AULAS/script.js
@@ -360,8 +360,52 @@
 
 // console.log(testeString);
 
-let nome = "111.1111.1111.11.";
+// let nome = "111.1111.1111.11.";
 
-nome = nome.replace(".", "");
+// nome = nome.replace(".", "");
 
-console.log(nome);
+// console.log(nome);
+
+// const faltaVisitar = (paisesVisitados) => {
+//   let totalPaises = 193;
+//   return `Falta visitar ${totalPaises - paisesVisitados}países`;
+// };
+
+// console.log(faltaVisitar(110));
+
+// var videoGames = ["Switch", "PS4", "XBox", "3DS"];
+// for (var i = 0; i < videoGames.length; i++) {
+//   if (videoGames[i] === "XBox") {
+//     console.log(videoGames[i]);
+//     break;
+//   }
+// }
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+
+// let anosCampeao = ["1959", "1962", "1970", "1994", "2002"];
+
+// // Interaja com a array utilizando um loop, para mostrar
+// // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+// anosCampeao.forEach((ano) => {
+//   return console.log(`O brasil ganhou a copa de ${ano}`);
+// });
+
+// // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+// var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+
+// for (i = 0; i < frutas.length; i++) {
+//   if (frutas[i] === "Pera") {
+//     console.log(frutas[i]);
+//     break;
+//   }
+// }
+
+// // Coloque a última fruta da array acima em uma variável,
+// // sem remover a mesma da array.
+
+// let ultimaFruta = frutas[frutas.length - 1];
+
+// console.log(ultimaFruta);
