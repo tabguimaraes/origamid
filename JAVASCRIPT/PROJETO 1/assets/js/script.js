4729a08de424f624efd29d487e25c0e6ddcb281a
TabGuima <tab.guimaraes@gmail.com>
2023-06-26 20:28:15 -0300

    revisao arrow functions

--- JAVASCRIPT/PROJETO 1/assets/js/script.js
+++ JAVASCRIPT/PROJETO 1/assets/js/script.js
@@ -27,27 +27,27 @@ console.log(animaisDescricao[0].innerHTML);
 // EXERCICIOS
 // Retorne no console todas as imagens do site
 
-console.log(html.querySelectorAll("img"));
-console.log("");
+// console.log(html.querySelectorAll("img"));
+// console.log("");
 // console.log(html.getElementsByTagName("img"));
 
 // // Retorne no console apenas as imagens que começaram com a palavra imagem
 
-console.log(html.querySelectorAll("[src*='imagem']"));
+// console.log(html.querySelectorAll("[src*='imagem']"));
 
 // // Selecione todos os links internos (onde o href começa com #)
-console.log(html.querySelectorAll("[href*='#']"));
+// console.log(html.querySelectorAll("[href*='#']"));
 
 // // Selecione o primeiro h2 dentro de .animais-descricao
-console.log(html.querySelector(".animais-descricao h2"));
+// console.log(html.querySelector(".animais-descricao h2"));
 
 // // Selecione o último p do site
-console.log(html.querySelectorAll(".copy p").innerHTML); //Não extrai o texto / valor de 'p'
-console.log(html.querySelector(".copy p").innerHTML);
+// console.log(html.querySelectorAll(".copy p").innerHTML); //Não extrai o texto / valor de 'p'
+// console.log(html.querySelector(".copy p").innerHTML);
 
 const paragrafos = html.querySelectorAll("p");
-console.log(paragrafos[paragrafos.length - 1].innerText);
-console.log(paragrafos[--paragrafos.length].innerHTML);
+// console.log(paragrafos[paragrafos.length - 1].innerText);
+// console.log(paragrafos[--paragrafos.length].innerHTML);
 
 // const footer = html.querySelectorAll(".copy p");
 // // console.log(footer);
@@ -60,3 +60,36 @@ console.log(paragrafos[--paragrafos.length].innerHTML);
 //   }
 //   console.log(item);
 // });
+
+// const titulos = Array.from(html.getElementsByClassName("titulo"));
+
+// titulos.forEach((item) => {
+//   console.log(item.innerHTML);
+// });
+
+// Mostre no console cada parágrado do site
+// console.log(paragrafos);
+
+paragrafos.forEach((item) => {
+  console.log(item);
+});
+
+// Mostre o texto dos parágrafos no console
+
+paragrafos.forEach((item) => {
+  console.log(item.innerText);
+});
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
