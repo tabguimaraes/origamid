c2c24cc1231eb661b69868d378550c1084b515c0
TabGuima <tab.guimaraes@gmail.com>
2023-06-29 13:22:43 -0300

    aula accordion list

--- JAVASCRIPT/PROJETO 1/assets/js/script.js
+++ JAVASCRIPT/PROJETO 1/assets/js/script.js
@@ -1,26 +1,42 @@
 function main() {
-  console.log("Conexão ok!");
+  console.log("online...");
 
-  const html = document.querySelector("html");
+  const activeClass = "ativo";
+  const animaisDescricao = document.querySelectorAll(".animais-descricao section");
+  const animaisLista = document.querySelectorAll(".animais-lista li");
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
-  const animaisDescricao = html.querySelectorAll(".animais-descricao section");
+  function initSessaoAnimais() {
+    if (animaisDescricao.length && animaisLista.length) {
+      function sessaoAnimais(index) {
+        animaisDescricao.forEach((item) => {
+          item.classList.remove(activeClass);
+        });
 
-  const animaisLista = html.querySelectorAll(".animais-lista li");
+        animaisDescricao[index].classList.add(activeClass);
+      }
 
-  if (animaisDescricao.length && animaisLista.length) {
-    function sessaoAnimais(index) {
-      animaisDescricao.forEach((item) => {
-        item.classList.remove("ativo");
+      animaisLista.forEach((item, index) => {
+        item.addEventListener("click", () => {
+          sessaoAnimais(index);
+        });
       });
-
-      animaisDescricao[index].classList.add("ativo");
     }
-
-    animaisLista.forEach((item, index) => {
-      item.addEventListener("click", () => {
-        sessaoAnimais(index);
-      });
-    });
   }
+  initSessaoAnimais();
+
+  function initAccordionList() {
+    if (accordionList.length) {
+      function activeAccordion() {
+        this.classList.toggle(activeClass);
+        this.nextElementSibling.classList.toggle(activeClass);
+      }
+
+      accordionList.forEach((item) => {
+        item.addEventListener("click", activeAccordion);
+      });
+    }
+  }
+  initAccordionList();
 }
 main();
