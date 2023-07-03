29ac1557e29b3057789ebe04e530033f5e7ee3ce
TabGuima <tab.guimaraes@gmail.com>
2023-07-03 08:08:51 -0300

    add function animacroll. testes ok

--- JAVASCRIPT/PROJETO 1/assets/js/script.js
+++ JAVASCRIPT/PROJETO 1/assets/js/script.js
@@ -8,12 +8,13 @@ function main() {
   const animaisDescricao = document.querySelectorAll(".animais-descricao section");
   const animaisLista = document.querySelectorAll(".animais-lista li");
   const section = document.querySelectorAll(".js-sections");
+  const valorWindow = window.innerHeight * 0.6;
   // const menuLi = document.querySelectorAll(".menu li a");
 
   function initSessaoAnimais() {
     if (animaisDescricao.length && animaisLista.length) {
       function sessaoAnimais(index) {
-        animaisDescricao.forEach((item) => {
+        animaisDescricao.forEach(item => {
           item.classList.remove(activeClass);
         });
 
@@ -36,50 +37,29 @@ function main() {
         this.nextElementSibling.classList.toggle(activeClass);
       }
 
-      accordionList.forEach((item) => {
+      accordionList.forEach(item => {
         item.addEventListener("click", activeAccordion);
       });
     }
   }
   initAccordionList();
 
-  // function initAnimaScroll() {
-  //   if (section.length) {
-  //     const valorWindow = window.innerHeight * 0.6;
-  //     console.log(valorWindow);
+  function initAnimaScroll() {
+    function AnimaScroll(item) {
+      const sectionTop = item.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - valorWindow < 0;
+      if (isSectionVisible) {
+        item.classList.add(activeClass);
+      }
+    }
 
-  //     section.forEach((item) => {
-  //       // console.log(isSectionVisible < 0);
-
-  //       item.addEventListener("scroll", (event) => {
-  //         console.log(event);
-  //         // const isSectionVisible = sectionTop - valorWindow;
-  //         // const sectionTop = item.getBoundingClientRect().top;
-  //         // if (isSectionVisible < 0) {
-  //         //   item.classList.add(activeClass);
-  //         // } else {
-  //         //   item.classList.remove(activeClass);
-  //         // }
-  //       });
-  //     });
-
-  //     // function animaScroll() {}
-  //     //VERIFICAR NECESSIDADE DE ATIVAR FUNÇÃO animaScroll()
-  //     // animaScroll();
-  //   }
-  // }
-  // initAnimaScroll();
-
-  const tamanhoTela = window.innerHeight * 0.6;
-  console.log(tamanhoTela);
-
-  section.forEach((item) => {
-    // const alturaTopo = item.getBoundingClientRect().top;
-    window.addEventListener("scroll", () => {
-      const alturaTopo = item.getBoundingClientRect().top;
-      console.log(alturaTopo);
+    section.forEach(item => {
+      window.addEventListener("scroll", () => {
+        AnimaScroll(item);
+      });
     });
-  });
+  }
+  initAnimaScroll();
 }
 
 main();
