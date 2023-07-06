f19a1400a1636f2065ad5541c2810e202e1b0f96
TabGuima <tab.guimaraes@gmail.com>
2023-07-06 09:35:37 -0300

    Exercícios Aula 0402 Prototype

--- JAVASCRIPT/AULAS/assets/js/script.js
+++ JAVASCRIPT/AULAS/assets/js/script.js
@@ -862,3 +862,44 @@ linksMenu[0].classList.add("ativo");
 
 // const novoTeste = new novoObjeto(2000, 1.5);
 // console.log(novoTeste);
+
+// Aula 0402 Prototype
+
+function Prototipo(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+console.log(Prototipo);
+
+Prototipo.prototype.andar = function () {
+  return this.nome + " andou";
+};
+
+const joao = new Prototipo("João", 25);
+// console.log(Prototipo.prototype);
+console.log(joao.andar());
+
+// Exercícios Aula 0402 Prototype
+
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
