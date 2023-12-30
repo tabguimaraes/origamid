bd2184ea652cb5b93ad0f327b3fd6c99d73d7d69
TabGuima <tab.guimaraes@gmail.com>
2023-12-29 23:25:50 -0300

    ajuste geral de código

--- JAVASCRIPT/EXERCICIOS/BITCOIN_INFO_TRACKER/assets/js/main.js
+++ JAVASCRIPT/EXERCICIOS/BITCOIN_INFO_TRACKER/assets/js/main.js
@@ -1,17 +1,11 @@
-const valorBitcoin = document.querySelector("#valorBitcoin");
-
 setInterval(() => {
+  const valorBitcoin = document.querySelector("#valorBitcoin");
   const buscaValor = `https://blockchain.info/ticker`;
+
   fetch(buscaValor).then((response) => {
-    response
-      .json()
-      .then((response) => {
-        return response.BRL;
-      })
-      .then((response) => {
-        const precoCompra = response.buy;
-        console.log(precoCompra);
-        valorBitcoin.innerText = `O preço de compra atual é de ${precoCompra.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`;
-      });
+    response.json().then((response) => {
+      const precoCompra = response.BRL.buy;
+      valorBitcoin.innerText = `Um bitcoin custa ${precoCompra.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`;
+    });
   });
 }, 30000);
