5acf00444754155dfa6a034fdfcbd510a0d2f9b7
Tiago Guimarães <tab.guimaraes@gmail.com>
2024-01-09 21:04:47 -0300

    misc

--- JAVASCRIPT/EXERCICIOS/BITCOIN_INFO_TRACKER/assets/js/main.js
+++ JAVASCRIPT/EXERCICIOS/BITCOIN_INFO_TRACKER/assets/js/main.js
@@ -1,11 +1,16 @@
-setInterval(() => {
+function init() {
   const valorBitcoin = document.querySelector("#valorBitcoin");
   const buscaValor = `https://blockchain.info/ticker`;
+  const updatePrice = document.querySelector("#update");
 
-  fetch(buscaValor).then((response) => {
-    response.json().then((response) => {
-      const precoCompra = response.BRL.buy;
-      valorBitcoin.innerText = `Um bitcoin custa ${precoCompra.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`;
+  updatePrice.addEventListener("click", () => {
+    fetch(buscaValor).then((response) => {
+      response.json().then((response) => {
+        const precoCompra = response.BRL.buy;
+        valorBitcoin.innerText = `Um bitcoin custa ${precoCompra.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`;
+      });
     });
   });
-}, 30000);
+}
+
+init();
